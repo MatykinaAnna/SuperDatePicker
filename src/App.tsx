0c97b8124e515f9feb4db3bc29ba687c1a24a91e
MatykinaAnna <matykina2000@gmail.com>
2025-06-29 15:42:07 +0300

    creater calendar

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { DatePicker } from './DatePicker';
+
+import './App.css';
+
+const MIN_DATE = new Date(2000, 1, 1);
+const MAX_DATE = new Date(2100, 1, 1);
+
+function App() {
+  const [date, setDate] = useState(() => new Date());
+
+  console.log(date.toDateString(), 'App');
+
+  return (
+    <div>
+      <DatePicker
+        value={date}
+        onChange={setDate}
+        min={MIN_DATE}
+        max={MAX_DATE}
+      />
+    </div>
+  );
+}
+
+export default App;
